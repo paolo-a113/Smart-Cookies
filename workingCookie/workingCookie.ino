cf9e1c3c7ba2cd24b740c00467d37dfe66634f84
paolo-a113 <pargu113@gmail.com>
2020-09-10 00:51:10 -0700

    precision

--- workingCookie/workingCookie.ino
+++ workingCookie/workingCookie.ino
@@ -10,7 +10,14 @@
 #include <WiFiClient.h>
 #include <ESP8266HTTPClient.h>
 
-Adafruit_Si7021 sensor = Adafruit_Si7021();
+//Adafruit_Si7021 sensor = Adafruit_Si7021();
+//Receiver code  
+  char cString[20];
+  byte chPos = 0;
+  byte ch = 0;
+
+  String readString;
+
 
 /* Put your SSID & Password */
 const char* ssid = "Smart Cookies";  // Enter SSID here
@@ -21,29 +28,32 @@ AsyncWebServer server(80);
 WebSocketsServer webSocket1 = WebSocketsServer(81);
 
 void getData() {
-  float t = sensor.readTemperature();
-  float h = sensor.readHumidity();
 
-  if (isnan(t) || t==998 || isnan(h) || h==998 ) {    
-    Serial.println("Failed to read from sensor!");
+ 
+   
+//  float t = sensor.readTemperature();
+//  float h = sensor.readHumidity();
+//
+//  if (isnan(t) || t==998 || isnan(h) || h==998 ) {    
+//    Serial.println("Failed to read from sensor!");
+//    return;
+//  }
+//  else {
+////    Serial.println(t);
+//    char tBuf[10];
+//    char hBuf[10];
+//    char msgBuf[10];
+//
+//    dtostrf(t, 4, 2, tBuf);
+//    dtostrf(h, 4, 2, hBuf);
+//    strcat(msgBuf,tBuf);
+//    strcat(msgBuf," ");
+//    strcat(msgBuf,hBuf);
+//
+//    Serial.println(msgBuf);
+    webSocket1.broadcastTXT(cString);
     return;
-  }
-  else {
-//    Serial.println(t);
-    char tBuf[10];
-    char hBuf[10];
-    char msgBuf[10];
-
-    dtostrf(t, 4, 2, tBuf);
-    dtostrf(h, 4, 2, hBuf);
-    strcat(msgBuf,tBuf);
-    strcat(msgBuf," ");
-    strcat(msgBuf,hBuf);
-
-    Serial.println(msgBuf);
-    webSocket1.broadcastTXT(msgBuf);
-    return;
-  }  
+//}  
 }
 
 
@@ -51,22 +61,12 @@ void getData() {
 unsigned long previousMillis = 0;        // will store last time LED was updated
 
 // constants won't change:
-const long interval = 100;           // interval at which to blink (milliseconds)
+const long interval = 2000;           // interval at which to blink (milliseconds)
 
 
 void setup(){
   Serial.begin(115200);
   Serial.println("SMART COOKIE - SI7021");
-
-  bool status;
-  status = sensor.begin();
-  if (!status) {
-      Serial.println("COULD NOT FIND SENSOR");
-      while (1);
-  }
-  else {
-    Serial.println("SENSOR WORKING");
-  }
 
   // Initialize SPIFFS
   if(!SPIFFS.begin()){
@@ -115,10 +115,25 @@ void loop(){
 
   unsigned long currentMillis = millis();
 
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-    getData();
-  }  
+//  if (currentMillis - previousMillis >= interval) {
+//    previousMillis = currentMillis;
+//    getData();
+//  }  
 
+if (Serial.available())  {
+    char c = Serial.read();  //gets one byte from serial buffer
+    if (c == '\n') {  //looks for end of data packet marker
+      Serial.read(); //gets rid of following \r
+      Serial.println(readString); //prints string to serial port out
+      readString.toCharArray(cString, 20);
+         webSocket1.broadcastTXT(cString);
+      //do stuff with captured readString
+     
+      readString=""; //clears variable for new input     
+     } 
+    else {     
+      readString += c; //makes the string readString
+    }
+  }
   
 }
